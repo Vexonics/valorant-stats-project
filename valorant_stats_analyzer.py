6239091d0bcca460901ee468a5a366b2d5dc9297
Adom Boateng <70593871+Vexonics@users.noreply.github.com>
2025-11-24 14:07:14 -0500

    valorant_stats_analyzer.py

--- valorant_stats_analyzer.py
+++ valorant_stats_analyzer.py
@@ -17,16 +17,13 @@ class ValorantStatsAnalyzer:
 
     def process_stats(self):
         stats = []
-
         for match in self.matches:
             players = match["players"]["all_players"]
-
             for p in players:
                 if p["puuid"].lower() == self.puuid.lower():
                     agent = p["character"]
                     won = random.uniform(0,1)
                     stats.append([agent, won])
-
         self.agent_stats = pd.DataFrame(stats, columns=["Agent", "Win"])
 
     def calculate_win_rates(self):
